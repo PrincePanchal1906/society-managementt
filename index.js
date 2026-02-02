9e4f44e63571a02ba1933b525e174abb08ef5ad8
princepanchal1906 <princepanchal2002@gmail.com>
2026-02-02 11:13:22 +0530

    o to i

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 
-// app.use("/",staticRoutes); hellooooo
+// app.use("/",staticRoutes); helliiiiiii
 app.use("/", userRoutes);
 app.use("/block", blockRoutes);
 app.use("/flat", flatRoutes);

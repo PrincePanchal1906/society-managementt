5e05c6f755f98e84feb266c9cd0fe0d3e96c531f
princepanchal1906 <princepanchal2002@gmail.com>
2026-02-02 11:37:27 +0530

    clear

--- index.js
+++ index.js
@@ -42,8 +42,7 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 
-// app.use("/",staticRoutes); helliiiiiii
-//hellllllll
+// app.use("/",staticRoutes); hellii0000
 app.use("/", userRoutes);
 app.use("/block", blockRoutes);
 app.use("/flat", flatRoutes);
